78551fbee034dfbf518fa1909f3986d65c0bdd05
hellopoisonx <x1665341912@gmail.com>
2023-05-21 12:32:11 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PIDS=$(ps -ef | rg "lemonbar -" | rg -v rg | awk '{print $2}')
 if [ ${#PIDS} == 0 ]; then
-  python ~/lemonbar/__init__.py
+  lua ~/lemonbar_lua/bar.lua
 else
   killall lemonbar
 fi
